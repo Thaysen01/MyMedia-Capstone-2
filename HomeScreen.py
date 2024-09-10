fb72e43670fdf0fdf37b0101d0dc06840b111b8c
Caleb Schoenberg <caleb.schoenberg@hotmail.com>
2024-09-09 17:45:58 -0700

    Created baseline for the rest of the application

---
+++ HomeScreen.py
@@ -0,0 +1,41 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6 import uic
+from MovieSelection import MovieSelectionScreen
+from BookSelection import BookSelectionScreen
+from MusicSelection import MusicSelectionScreen
+import Constants
+
+class HomeScreen(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi("ui/homeScreen.ui", self)
+
+        # Add pages to stacked widget
+        self.movieSelection = MovieSelectionScreen()
+        self.stackedWidget.addWidget(self.movieSelection) 
+
+        self.musicSelection = MusicSelectionScreen()
+        self.stackedWidget.addWidget(self.musicSelection)
+
+        self.bookSelection = BookSelectionScreen()
+        self.stackedWidget.addWidget(self.bookSelection)
+
+        # Connect buttons
+        self.moviesButton.clicked.connect(self.showMovies)
+        self.musicButton.clicked.connect(self.showMusic)
+        self.booksButton.clicked.connect(self.showBooks)
+
+    def showMovies(self):
+        self.movieSelection.tabWidget.setCurrentIndex(Constants.LIBRARY_INDEX)
+        self.stackedWidget.setCurrentIndex(Constants.MOVIE_SELECTION_SCREEN_INDEX)
+
+    def showMusic(self):
+        self.musicSelection.tabWidget.setCurrentIndex(Constants.LIBRARY_INDEX)
+        self.stackedWidget.setCurrentIndex(Constants.MUSIC_SELECTION_SCREEN_INDEX)
+
+    def showBooks(self):
+        self.bookSelection.tabWidget.setCurrentIndex(Constants.LIBRARY_INDEX)
+        self.stackedWidget.setCurrentIndex(Constants.BOOK_SELECTION_SCREEN_INDEX)
+
