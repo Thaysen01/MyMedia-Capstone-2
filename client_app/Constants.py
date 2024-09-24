10f0575d873bb09c7ee1017fbe441767d133e0bc
Caleb Schoenberg <caleb.schoenberg@hotmail.com>
2024-09-24 17:50:02 -0500

    added ability to switch between login screen and home in screen. Minor home screen updates

--- client_app/Constants.py
+++ client_app/Constants.py
@@ -1,13 +1,19 @@
+# ==================================================================
 # Main app stacked widget (login, home, play movie, read book) indexes
+# ==================================================================
 LOGIN_SCREEN_INDEX = 0
 HOME_SCREEN_INDEX = 1
 
+# ==================================================================
 # Home screen stacked widget indexes
+# ==================================================================
 MOVIE_SELECTION_SCREEN_INDEX = 0
 MUSIC_SELECTION_SCREEN_INDEX = 1
 BOOK_SELECTION_SCREEN_INDEX = 2
 SETTINGS_SCREEN_INDEX = 3
 
+# ==================================================================
 # Tab Widget Indexes
+# ==================================================================
 LIBRARY_INDEX = 0
 PLAYLISTS_INDEX = 0
--- client_app/client_app.py
+++ client_app/client_app.py
@@ -24,11 +24,24 @@ class MainWindow(QMainWindow):
         self.stackedWidget.addWidget(self.homeScreen)
 
         # Connect buttons
-        self.loginScreen.loginButton.clicked.connect(self.goToHome) # clicking login button takes you to movie selection screen
+        self.loginScreen.loginButton.clicked.connect(self.loginButtonClicked) # clicking login button takes you to movie selection screen
+        self.homeScreen.logoutButton.clicked.connect(self.logoutButtonClicked)
 
     def goToHome(self):
-        self.homeScreen.showMovies()
         self.stackedWidget.setCurrentIndex(Constants.HOME_SCREEN_INDEX)
+
+    def goToLogin(self):
+        self.stackedWidget.setCurrentIndex(Constants.LOGIN_SCREEN_INDEX)
+
+    def loginButtonClicked(self):
+        # This will eventually handle accounts and whatnot
+        self.loginScreen.usernameEdit.clear()
+        self.loginScreen.passwordEdit.clear()
+        self.homeScreen.showMovies()
+        self.goToHome()
+    
+    def logoutButtonClicked(self):
+        self.goToLogin()
 
 if __name__ == '__main__': 
     app = QApplication(sys.argv)
