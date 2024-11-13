4e194f86ecead87f0ea09309873f53a3d1d3ec20
Caleb Schoenberg <caleb.schoenberg@hotmail.com>
2024-11-12 19:11:08 -0600

    Fixed things

--- Database/DatabaseFunctions.py
+++ Database/DatabaseFunctions.py
@@ -1,41 +1,45 @@
 import sqlite3
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'MyMediaData.db')
 
 def getMoviePath(movieID):
     # Gets the filepath of a movie given its id
 
-    con = sqlite3.connect('MyMediaData.db')
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
     res = cur.execute(f'SELECT filePath FROM coreInfo WHERE ID={movieID}')
     path = res.fetchall()[0]
     con.close()
-    return path
+    return path[0]
 
 def getMovieList():
     # Returns a list of movie names from the database
 
-    con = sqlite3.connect('MyMediaData.db')
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
-    res = cur.execute(f'SELECT movieName FROM movies')
-    movieList = res.fetchall()[0]
+    res = cur.execute(f'SELECT title FROM coreInfo WHERE type="movie"')
+    movieList = res.fetchall()
     con.close()
-    return list(movieList[0])
+    return [movie[0] for movie in movieList]
 
 def getMovieIDList():
     # Returns a list of movie ids from the database
 
-    con = sqlite3.connect('MyMediaData.db')
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
-    res = cur.execute(f'SELECT ID FROM movies')
-    movieIDList = res.fetchall()[0]
+    res = cur.execute(f'SELECT ID FROM coreInfo WHERE type="movie"')
+    movieIDList = res.fetchall()
     con.close()
-    return list(movieIDList[0])
+    return [movieID[0] for movieID in movieIDList]
     
 def getMovieImageList():
     # Returns a list of movie image paths from the database
     
-    con = sqlite3.connect('MyMediaData.db')
+    con = sqlite3.connect(db_path)
     cur = con.cursor()
-    res = cur.execute(f'SELECT imagePath FROM movies')
-    movieImageList = res.fetchall()[0]
+    res = cur.execute(f'SELECT imagePath FROM coreInfo WHERE type="movie"')
+    movieImageList = res.fetchall()
     con.close()
-    return list(movieImageList[0])
+    return [movieImage[0] for movieImage in movieImageList]
